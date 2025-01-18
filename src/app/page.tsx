cd3f34bd7c83e49063caf36bb7110e9284a97dcd
hien <hienvuongnhat@gmail.com>
2025-01-19 04:11:43 +0700

    Add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {useState} from "react";
 
 export default function Home() {
@@ -9,10 +10,26 @@ export default function Home() {
             <div
                 className="2xl:container 2xl:mx-auto w-full">
                 <div className={`grid w-full h-full gap-4 ${state ? "grid-cols-4" : "grid-cols-3"}`}>
-                    <div className={"shadow-lg border rounded-lg bg-background"}></div>
-                    <div className={"col-span-2 shadow-lg border rounded-lg bg-background"}></div>
+                    {/*<div className={"shadow-lg border rounded-lg bg-background"}></div>*/}
+                    <Card>
+                        {/*<CardHeader>*/}
+                        {/*    <CardTitle>Card Title</CardTitle>*/}
+                        {/*    <CardDescription>Card Description</CardDescription>*/}
+                        {/*</CardHeader>*/}
+                        {/*<CardContent>*/}
+                        {/*    <p>Card Content</p>*/}
+                        {/*</CardContent>*/}
+                        {/*<CardFooter>*/}
+                        {/*    <p>Card Footer</p>*/}
+                        {/*</CardFooter>*/}
+                    </Card>
+
+                    <Card className="col-span-2">
+                    </Card>
+                    {/*<div className={"col-span-2 shadow-lg border rounded-lg bg-background"}></div>*/}
                     {state ? (
-                        <div className={"shadow-lg border rounded-lg bg-background"}></div>
+                        <Card>
+                        </Card>
                     ) : null}
                 </div>
             </div>
