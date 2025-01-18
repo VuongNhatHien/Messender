62b39e282f451093d36d9ddc9e8021251c1206a1
hien <hienvuongnhat@gmail.com>
2025-01-19 04:40:30 +0700

    Add header for first card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,29 @@
-"use client"
+"use client";
 
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {useState} from "react";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
     const [state] = useState(true);
+
     return (
         <div className="flex flex-grow px-8 py-5 bg-secondary">
-            <div
-                className="2xl:container 2xl:mx-auto w-full">
+            <div className="2xl:container 2xl:mx-auto w-full">
                 <div className={`grid w-full h-full gap-4 ${state ? "grid-cols-4" : "grid-cols-3"}`}>
-                    {/*<div className={"shadow-lg border rounded-lg bg-background"}></div>*/}
-                    <Card>
-                        {/*<CardHeader>*/}
-                        {/*    <CardTitle>Card Title</CardTitle>*/}
-                        {/*    <CardDescription>Card Description</CardDescription>*/}
-                        {/*</CardHeader>*/}
-                        {/*<CardContent>*/}
-                        {/*    <p>Card Content</p>*/}
-                        {/*</CardContent>*/}
-                        {/*<CardFooter>*/}
-                        {/*    <p>Card Footer</p>*/}
-                        {/*</CardFooter>*/}
-                    </Card>
-
-                    <Card className="col-span-2">
-                    </Card>
-                    {/*<div className={"col-span-2 shadow-lg border rounded-lg bg-background"}></div>*/}
-                    {state ? (
-                        <Card>
-                        </Card>
-                    ) : null}
+                    <div className="card">
+                        <div className={"flex justify-between items-center"}>
+                            <p className={"text-xl font-bold"}>Chats</p>
+                            <Button variant={"outline"} className={"rounded-full"} size={"icon"}>
+                                <p className={"text-2xl"}>
+                                    +
+                                </p>
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="card col-span-2"></div>
+                    {state && <div className="card"></div>}
                 </div>
             </div>
         </div>
-
     );
 }
