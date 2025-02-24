4068f8ce72917a7e343289c474f66ad1fb9c74db
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-24 11:08:42 +0700

    Add error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,34 @@
+"use client"; // Error boundaries must be Client Components
+
+import { useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className="flex h-full flex-col items-center justify-center gap-4">
+            <Image src="/logo.png" width={120} height={120} alt="Logo" />
+            <div className="text-4xl">{`There was an error loading this page`}</div>
+            <div className="text-lg text-muted-foreground">
+                An unexpected error occurred. Please try again later
+            </div>
+            <Button
+                variant="default"
+                className="rounded-full"
+                onClick={() => reset}
+            >
+                Try again
+            </Button>
+        </div>
+    );
+}
