4a3abec2863f8640f2519c8e6950e5f221f8b8b5
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-26 21:46:08 +0700

    Track fetcher again

---
+++ src/lib/fetcher.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import envConfig from "@/config";
+
+export const fetcher = async (url: string) => {
+    const baseUrl = envConfig.NEXT_PUBLIC_API_ENDPOINT;
+    const fullUrl = url.startsWith("/")
+        ? `${baseUrl}${url}`
+        : `${baseUrl}/${url}`;
+    return fetch(fullUrl, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    })
+        .then((res) => res.json())
+        .then((result) => result.data);
+};
