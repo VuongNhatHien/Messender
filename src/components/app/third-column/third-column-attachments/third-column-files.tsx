3943aeb7a039f966b52ca08697a5203cbfd0503d
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-03-05 15:48:34 +0700

    Clean to run build

--- src/components/app/third-column/third-column-attachments/third-column-files.tsx
+++ src/components/app/third-column/third-column-attachments/third-column-files.tsx
@@ -9,21 +9,23 @@ import { Button } from "../../../ui/button";
 
 const FileItem = ({ file }: { file: AttachmentType }) => {
     return (
-        <a
-            href={file.url}
-            target="_blank"
-            className="hover-custom flex items-center gap-2 rounded-sm py-1 pe-2"
-        >
-            <div className="flex size-[52px] shrink-0 items-center justify-center rounded-xl bg-muted">
-                <File size={36} className="" />
-            </div>
-            <div className="overflow-hidden">
-                <p className="truncate font-extrabold">{file.name}</p>
-                <p className="font-medium text-muted-foreground">
-                    {formatFileSize(file.size!)}
-                </p>
-            </div>
-        </a>
+        file.url && (
+            <a
+                href={file.url}
+                target="_blank"
+                className="hover-custom flex items-center gap-2 rounded-sm py-1 pe-2"
+            >
+                <div className="flex size-[52px] shrink-0 items-center justify-center rounded-xl bg-muted">
+                    <File size={36} className="" />
+                </div>
+                <div className="overflow-hidden">
+                    <p className="truncate font-extrabold">{file.name}</p>
+                    <p className="font-medium text-muted-foreground">
+                        {formatFileSize(file.size!)}
+                    </p>
+                </div>
+            </a>
+        )
     );
 };
 
